f0f076e6252dec95675aef1e3999ab8e3be1c45b
= <=>
2024-05-28 10:34:31 -0300

    Conta Banco com Scanner

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,12 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        
+        double nota = 10.0;
+
+        if(nota>5) {
+            System.out.println("Aprovado!");
+        } else {
+            System.out.println("Reprovado!");
+        }
+    }
+}

dad779e0581eb568dc1cf28b47cb57bf9fa73cdb
= <=>
2024-05-31 17:29:33 -0300

    if e else + switch case

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,10 +1,17 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
         
-        double nota = 10.0;
+        //Operador ternário: 
+        //int nota = 7;
+        //String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        //System.out.println(resultado);
 
-        if(nota>5) {
+        double nota = 5.0;
+
+        if(nota>=7) {
             System.out.println("Aprovado!");
+        } else if (nota>=5 && nota<7) {
+            System.out.println("Prova de recuperação!");
         } else {
             System.out.println("Reprovado!");
         }
